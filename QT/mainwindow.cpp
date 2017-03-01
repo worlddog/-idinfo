ab39fb2ac43d1e894b15e8f3a8bbc87dcfbdb8f0
DOG <weiyiweiyiweiyiyi@163.com>
2017-03-01 15:03:52 +0800

    添加自适应二值阈值化函数

--- QT/mainwindow.cpp
+++ QT/mainwindow.cpp
@@ -28,15 +28,83 @@ void MainWindow::on_pushButton_clicked()
 
 	QString imagefile = QFileDialog::getOpenFileName(this,"打开图像",QDir::currentPath(),"Document files (*.jpg *.bmp);;All files(*.*)");
 	
-	this->image = cvLoadImage(imagefile.toLocal8Bit().data(), 1);
+	if (!imagefile.isNull())
+	{
+		
 
-	//	IplImage * test; test = cvLoadImage(filename.toLocal8Bit().data());
+		this->image = cvLoadImage(imagefile.toLocal8Bit().data(), 1);
+		//	IplImage * test; test = cvLoadImage(filename.toLocal8Bit().data());
+		this->qImage = new QImage(QSize(this->image->width, this->image->height), QImage::Format_RGB888);
+		this->tempImage = cvCreateImageHeader(cvSize(this->image->width, this->image->height), 8, 3);
+		this->tempImage->imageData = (char*)(this->qImage->bits());// tempImage->imageData指向的存放图像实体区域的指针, qlmage->bits() 是QT中 图像实体区域指针，现在让tempImage->imageData 也指向 qlmage->bits() 指向的区域
+		cvCopy(this->image, this->tempImage, 0);// 把图像数据从this->image 拷贝到 this->tempImage指向的区域，而this->tempImage此时指向的区域和this->qImage->bits()所指的是同一个区域
+		cvCvtColor(this->tempImage, this->tempImage, CV_BGR2RGB); //这个是颜色空间转换
+		this->ui->image_label->setPixmap((QPixmap::fromImage(*this->qImage)).scaled(this->ui->image_label->size()));// 这句话就是你想要的 把opencv处理出来的图片放到 qt 指定的方框里了
+	
+	
+		Ada_Thresgold(imagefile.toLocal8Bit().data());
+	
 
-	this->qImage = new QImage(QSize(this->image->width, this->image->height), QImage::Format_RGB888);
-	this->tempImage = cvCreateImageHeader(cvSize(this->image->width, this->image->height), 8, 3);
-	this->tempImage->imageData = (char*)(this->qImage->bits());// tempImage->imageData指向的存放图像实体区域的指针, qlmage->bits() 是QT中 图像实体区域指针，现在让tempImage->imageData 也指向 qlmage->bits() 指向的区域
-	cvCopy(this->image, this->tempImage, 0);// 把图像数据从this->image 拷贝到 this->tempImage指向的区域，而this->tempImage此时指向的区域和this->qImage->bits()所指的是同一个区域
-	cvCvtColor(this->tempImage, this->tempImage, CV_BGR2RGB); //这个是颜色空间转换
-	this->ui->image_label->setPixmap((QPixmap::fromImage(*this->qImage)).scaled(this->ui->image_label->size()));// 这句话就是你想要的 把opencv处理出来的图片放到 qt 指定的方框里了
+
+
+	
+	
+	
+	}
+	else
+	{
+		return;
+	}
+
+
+
+
+	
+
+
+
 
 }
+
+//1、获取图片R通道
+Mat MainWindow::getRplane(const Mat &in)
+{
+	vector<Mat> splitBGR(in.channels()); //容器大小为通道数3
+	split(in, splitBGR);
+	//return splitBGR[2];  //R分量
+
+	if (in.cols > 700 | in.cols >600)
+	{
+		Mat resizeR(450, 600, CV_8UC1);
+		cv::resize(splitBGR[2], resizeR, resizeR.size());
+
+		return resizeR;
+	}
+	else
+		return splitBGR[2];
+
+}
+
+
+//2,自适应二值阈值化
+void MainWindow::Ada_Thresgold(const char* filename)
+{
+	int adaptive_method = CV_ADAPTIVE_THRESH_MEAN_C ;
+	int threshold_type = CV_THRESH_BINARY;
+	int block_size = 75;
+	double offset = 15;
+	double param1 = 5;
+
+	IplImage *Igray = 0, *Iat;
+	
+	//输入单通道图像
+	Igray = cvLoadImage(filename, CV_LOAD_IMAGE_GRAYSCALE);
+	//创建输出图像
+	Iat = cvCreateImage(cvSize(Igray->width, Igray->height), IPL_DEPTH_8U, 1);
+	//二值化
+	cvAdaptiveThreshold(Igray, Iat,255, adaptive_method, threshold_type, block_size, offset);
+	//显示窗口
+	cvNamedWindow("Ada_Th", 1);
+	cvShowImage("Ada_Th", Iat);
+
+}    
--- QT/mainwindow.h
+++ QT/mainwindow.h
@@ -6,6 +6,14 @@
 #include <highgui.h> 
 #include<Qgraphicsview>
 
+#include <QWidget>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/ml/ml.hpp"
+using namespace cv; //使用OPENCV命名空间
+
 namespace Ui {
 class MainWindow;
 }
@@ -29,6 +37,35 @@ private:
 	QImage * qImage; // qt 中的图片类型指针
 	IplImage * image;  // opencv里的图片类型指针
 	IplImage * tempImage;
+	IplImage * image2;
+
+	IplImage *Igray, *Iat;
+
+	/////////////////////////////////////
+	Mat imgSrc;
+
+	Mat getRplane(const Mat & in);  //获得图片的R通道
+	void Ada_Thresgold(const char* filename);//自适应二值阈值化
+	void OstuBeresenThreshold(const Mat & in, Mat & out);
+	void posDetect(const Mat &in, vector<RotatedRect> & rects);
+	bool isEligible(const RotatedRect & candidate);
+	void normalPosArea(const Mat &intputImg, RotatedRect &rects_optimal, Mat& output_area);
+	void char_segment(const Mat & inputImg, vector <Mat>& dst_mat); //得到16*20的标准字符分割图像
+	void getAnnXML();  //获得神经网络的训练矩阵和标签矩阵，
+
+	void calcGradientFeat(const Mat& imgSrc, Mat& out);
+	float sumMatValue(const Mat& image);
+	Mat projectHistogram(const Mat& img, int t);
+
+	void ann_train(CvANN_MLP &ann, int numCharacters, int nlayers);
+	void classify(CvANN_MLP& ann, vector<Mat> &char_Mat, vector<int> & char_result);
+
+	void getParityBit(vector<int> & char_result);  //获得校验位
+
+	///////////////////////////////////////
+
+
+
 };
 
 #endif // MAINWINDOW_H
